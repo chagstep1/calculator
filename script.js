a2f837ca95a648da26ef6551fcce4354a531ee41
chagstep1 <yandex@chagstep1.ru>
2021-12-30 21:15:34 +0300

    Finish exercise

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ const MULTI = "*";
 const DIVIDE = "/";
 const CLEAR = "clear";
 const EQUAL = "=";
+const DOT = ".";
+const BACKSPACE = "<--"
 
 const ERROR = "ERROR";
 const DIVIDEBYZERO = "DIV BY ZERO";
 const OVERFLOW = "OVERFLOW";
 
 const MAX_INT_NUMBERS = 10;
-const MAX_POINT_NUMBERS = 2;
+const MAX_DEC_NUMBERS = 2;
 
 const displayDiv = document.querySelector("#display");
 
-let s_current_number = "";
-let s_last_number = "";
-let last_operator = null;
+let left_operant = null;
+let operator = null;
+let s_input = "";
 
 function add(num1, num2) {
 	return num1 + num2;
@@ -34,28 +36,29 @@ function divide(numFrom, numBy) {
 	return numFrom / numBy;
 }
 
-function operate(numLast, numNew, operator) {
+// Always compare return value to DIVIDEBYZERO and OVERFLOW first! aka not Number;
+// Returns opLeft on any non-standard case.
+function operate(opLeft, opRight, operator) {
 	let num;
 	switch (operator) {
 		case PLUS:
-			num = add(numLast, numNew);
+			num = add(opLeft, opRight);
 			break;
 		case MINUS:
-			num = substract(numLast, numNew);
+			num = substract(opLeft, opRight);
 			break;
 		case MULTI:
-			num = multiply(numLast, numNew);
+			num = multiply(opLeft, opRight);
 			break;
 		case DIVIDE:
-			num = divide(numLast, numNew);
+			if (opRight === 0) return DIVIDEBYZERO;
+			num = divide(opLeft, opRight);
 			break;
-		case NONE:
-			return numLast;
 		default:
-			alert("ERROR operate!");
-			break;
+			return opLeft;
 	}
-	num = (Math.round(num*(10 ** MAX_POINT_NUMBERS))/(10 ** MAX_POINT_NUMBERS);
+	if (num  >= 10 ** MAX_INT_NUMBERS) return OVERFLOW;
+	num = (Math.round(num*(10 ** MAX_DEC_NUMBERS))/(10 ** MAX_DEC_NUMBERS));
 	return num;
 }
 
@@ -69,10 +72,86 @@ function updateDisplay(num) {
 }
 
 function clearVarsAndDisplay(num) {
-	s_current_number = "";
-	s_last_number = "";
-	last_operator = null;
+	left_operant = null;
+	operator = null;
+	s_input = "";
 	updateDisplay(num);
+}
+
+function canInputMoreDigit() {
+	const dotIndex = s_input.indexOf('.');
+	return (dotIndex === -1) ? (s_input.length < MAX_INT_NUMBERS) : (s_input.length - dotIndex <= MAX_DEC_NUMBERS);
+}
+
+function onDigitPress(digit) {
+	if (operator === EQUAL)
+		clearVarsAndDisplay("");
+	
+	if (canInputMoreDigit()) {
+		s_input += digit;
+		updateDisplay(s_input);
+	}
+}
+
+function onDotPress() {
+	if (operator === EQUAL)
+		clearVarsAndDisplay("");
+	
+	if (s_input === "")
+	{
+		s_input = "0.";
+		updateDisplay(s_input);
+	}
+	else if (s_input.indexOf('.') === -1)
+	{
+		s_input += '.';
+		updateDisplay(s_input);
+	}
+}
+
+function onBackspacePress() {
+	if (operator === EQUAL)
+		clearVarsAndDisplay("");
+	
+	s_input = s_input.slice(0, -1);
+	updateDisplay(s_input);
+}
+
+function onEqualPress() {
+	if (left_operant !== null && operator !== null && s_input !== "") {		
+		left_operant = operate(Number(left_operant), Number(s_input), operator);
+		if (typeof left_operant !== "number") 
+		{
+			clearVarsAndDisplay(left_operant);
+			return;
+		}
+		
+		updateDisplay(left_operant);
+		operator = EQUAL;
+		s_input = "";
+	}
+}
+
+//does not account for EQUAL!
+function onOperatorPress(opButton) {
+	if (s_input !== "")
+	{
+		if (left_operant === null) {
+			left_operant = Number(s_input);
+		}
+		else {
+			left_operant = operate(Number(left_operant), Number(s_input), operator);
+			if (typeof left_operant !== "number") 
+			{
+				clearVarsAndDisplay(left_operant);
+				return;
+			}
+			updateDisplay(left_operant);
+		}
+	}
+	
+	s_input = "";
+	operator = opButton;
 }
 
 function pressedButton(button) {
@@ -87,15 +166,11 @@ function pressedButton(button) {
 		case '7':
 		case '8':
 		case '9':
-			if (last_operator === EQUAL)
-			{
-				s_current_number = "";
-				last_operator = null;
-			}
-			if (s_current_number.length >= MAX_INT_NUMBERS)
-				break;
-			s_current_number += button;
-			updateDisplay(s_current_number);
+			onDigitPress(button);
+			break;
+		
+		case DOT:
+			onDotPress();
 			break;
 			
 		case CLEAR:
@@ -106,52 +181,85 @@ function pressedButton(button) {
 		case MINUS:
 		case MULTI:
 		case DIVIDE:
-			if (s_current_number !== "")
-			{
-				if (s_last_number !== "" && last_operator !== null)
-				{
-					if (Number(s_current_number) === 0 && last_operator === DIVIDE) {
-						clearVarsAndDisplay(DIVIDEBYZERO);
-						break;
-					}
-						
-					s_last_number = operate(Number(s_last_number), Number(s_current_number), last_operator);
-					
-					/*let dotIndex = s_last_number.indexOf(".");
-					if (dotIndex !== -1 && s_last_number > s_last_number.substring(0, dotIndex))
-					{
-						clearVarsAndDisplay(OVERFLOW);
-						break;
-					}*/
-					
-					updateDisplay(s_last_number);
-				}
-				else {
-					s_last_number = s_current_number;
-				}
-				s_current_number = "";
-			}
-			last_operator = button;
+			onOperatorPress(button);
 			break;
 			
 		case EQUAL:
-			if (last_operator !== null && s_last_number !== "" && s_current_number !== "") {
-				if (Number(s_current_number) === 0 && last_operator === DIVIDE) {
-					clearVarsAndDisplay(DIVIDEBYZERO);
-					break;
-				}
-				
-				s_current_number = operate(Number(s_last_number), Number(s_current_number), last_operator);
-				updateDisplay(s_current_number);
-				s_last_number = "";
-				last_operator = EQUAL;
-			}
+			onEqualPress();
 			break;
+		
+		case DOT:
+			onDotPress();
+			break;
+		
+		case BACKSPACE:
+			onBackspacePress();
+			break;
+			
 		default:
 			alert("ERROR pressedButton!");
 			break;
 	}
 }
+
+document.addEventListener("keydown", function (event) {
+  if (event.defaultPrevented) {
+    return; // Do nothing if the event was already processed
+  }
+
+  switch (event.key) {
+	case "0":
+	case "1":
+	case "2":
+	case "3":
+	case "4":
+	case "5":
+	case "6":
+	case "7":
+	case "8":
+	case "9":
+		onDigitPress(Number(event.key));
+		break;
+	case "Add":
+	case "+":
+		onOperatorPress(PLUS);
+		break;
+	case "Subtract":
+	case "-":
+		onOperatorPress(MINUS);
+		break;
+	case "Multiply":
+	case "*":
+		onOperatorPress(MULTI);
+		break;
+	case "Divide":
+	case "/":
+		onOperatorPress(DIVIDE);
+		break;
+    case "Esc":
+    case "Escape":
+    case "Clear":
+    case "Delete":
+		clearVarsAndDisplay("");
+		break;
+	case ".":
+	case "Decimal":
+		onDotPress();
+		break;
+    case "Enter":
+	case "=":
+		onEqualPress();
+		break;
+    case "Backspace":
+		onBackspacePress();
+		break;
+	default:
+		return;
+  }
+
+  // Cancel the default action to avoid it being handled twice
+  event.preventDefault();
+}, true);
 
 document.querySelector("#button-0").addEventListener('click', () => pressedButton("0"));
 document.querySelector("#button-1").addEventListener('click', () => pressedButton("1"));
@@ -169,4 +277,6 @@ document.querySelector("#button-plus").addEventListener('click', () => pressedBu
 document.querySelector("#button-minus").addEventListener('click', () => pressedButton(MINUS));
 document.querySelector("#button-multi").addEventListener('click', () => pressedButton(MULTI));
 document.querySelector("#button-divide").addEventListener('click', () => pressedButton(DIVIDE));
+document.querySelector("#button-dot").addEventListener('click', () => pressedButton(DOT));
+document.querySelector("#button-backspace").addEventListener('click', () => pressedButton(BACKSPACE));
 
